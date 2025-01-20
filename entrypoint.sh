070538d7ea87b53c594760223b025ed176c529c6
BAC <41997195+bac0id@users.noreply.github.com>
2025-01-20 16:18:42 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
+nohup dockerd &
+
 docker pull portainer/portainer-ce
 
 docker volume create portainer_data
@@ -8,3 +10,5 @@ docker run -d -p 8000:8000 -p 9443:9443 -p 9000:9000 \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v portainer_data:/data \
     portainer/portainer-ce
+
+#python3 /app/app.py
